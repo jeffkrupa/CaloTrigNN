f5c37326634ebb2963f9b88b247bdb2111f9eaee
Jeffrey Krupa <jeffrey.krupa@cern.ch>
2019-03-11 22:48:16 +0100

    Add gen matching

--- DataFormats/interface/TPFPart.hh
+++ DataFormats/interface/TPFPart.hh
@@ -12,23 +12,25 @@ namespace baconhep
     public:
       TPFPart():
 	pt(0), eta(0), phi(0), m(0),e(0),ecalE(0),hcalE(0),avgdepth(0), ecalSum(0),
-	q(0),pfType(-1)
+	q(0),pfType(-1), 
+	genMatch(0)
     {}
     ~TPFPart(){}
 
     float pt,eta,phi,m,e,ecalE,hcalE,avgdepth,ecalSum;
     int   q,pfType;
+    bool  genMatch;
     std::array<double,7> depthE;
     std::array<double,7> depthgenE;
     std::array<double,7> depthgenESum;
     std::array<double,7> depthESum;
     std::array<double,7> depthFrac;
-    std::vector<float> fraction;
-    std::vector<int>   detid;
-    std::vector<int>   ieta;
-    std::vector<int>   iphi;
-    std::vector<float> depth;
-    std::vector<float> gene;
+    //std::vector<float> fraction;
+    //std::vector<int>   detid;
+    //std::vector<int>   ieta;
+    //std::vector<int>   iphi;
+    //std::vector<float> depth;
+    //std::vector<float> gene;
 
     ClassDef(TPFPart,4)
   };
